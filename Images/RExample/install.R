4aed99cd0d852618f91677276fcc2e52789cb735
L0ren2 <lorenz_schwab@web.de>
2022-12-06 08:25:40 +0100

    modified cli-module.R

--- Images/RExample/install.R
+++
@@ -1,6 +0,0 @@
-if (!requireNamespace("BiocManager", quietly = TRUE))
-    install.packages("BiocManager")
-
-BiocManager::install("RCurl")
-BiocManager::install("EBImage")
-BiocManager::install("Cardinal")
